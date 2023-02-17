53151846a0a31ce17b4397375b0477afe2cd0365
lonebhen <ben.angmortey@gmail.com>
2023-02-17 06:15:46 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,48 @@
 #!/usr/bin/python3
-'''a script that reads stdin line by line and computes metrics'''
 
-
+"""
+    a script that reads stdin line by line and computes metrics:
+"""
 import sys
 
-cache = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-counter = 0
+file_size = 0
+code = 0
+line_count = 0
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
+
+
+def print_res(codes, file_size):
+    print("File size: {}".format(file_size))
+    for key, value in sorted(codes.items()):
+        print("{}: {}".format(key, value))
+
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in cache.keys():
-                cache[code] += 1
-            total_size += size
-            counter += 1
+        read_line = line.split()
 
-        if counter == 10:
-            counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(cache.items()):
-                if value != 0:
-                    print('{}: {}'.format(key, value))
+        if len(read_line) > 2:
+            line_count += 1
 
-except Exception as err:
-    pass
+            if line_count <= 10:
+                file_size = int(read_line[-1])
+                code = read_line[-2]
+
+                if (code in codes.keys()):
+                    codes[code] += 1
+
+            if line_count == 10:
+                print_res(codes, file_size)
+                line_count = 0
 
 finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    print_res(codes, file_size)
