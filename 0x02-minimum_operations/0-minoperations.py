03c53fad995aab7bde39e594a82d92e684fe45e4
lonebhen <ben.angmortey@gmail.com>
2023-03-15 22:49:50 +0000

    min operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Calculate minimum operations"""
+
+
+def minOperations(n):
+    """min operations"""
+    if n == 1:
+        return 0
+
+    for i in range(2, n+1):
+        if n % i == 0:
+            return minOperations(n // i) + i
+
+    return 0
