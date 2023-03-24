f6839a8a7e4b2bee8c2ce8f7c045707413c1d822
lonebhen <ben.angmortey@gmail.com>
2023-03-24 05:02:45 +0000

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+
+"""
+    Making Change
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+
+"""
+
+
+def makeChange(coins, total):
+    '''Making change'''
+
+    if total < 0:
+        return -1
+
+    if coins is None or len(coins) == 0:
+        return -1
+
+    change = 0
+
+    all_coins = sorted(coins, reverse=True)
+
+    amount_rem = total
+
+    for coin in all_coins:
+        while (amount_rem % coin >= 0 and amount_rem >= coin):
+            change += int(amount_rem/coin)
+            amount_rem = amount_rem % coin
+
+    change = change if amount_rem == 0 else -1
+
+    return change
+
+
+# print(makeChange([1, 2, 25], 37))
